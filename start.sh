fd270a52a30b842b04044aedb526fffb68a90c99
valetzx <45054400+valetzx@users.noreply.github.com>
2023-02-21 14:50:14 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,44 @@
+chmod u+x main
+
+cat >/data/config.json <<EOF
+{
+  "force": false,
+  "address": "0.0.0.0",
+  "port": 5244,
+  "site_url": "",
+  "cdn": "",
+  "jwt_secret": "2BmIS5GkuHwx0Nws",
+  "token_expires_in": 48,
+  "database": {
+    "type": "$DB_TYPE",
+    "host": "$DB_HOST",
+    "port": $DB_PORT,
+    "user": "$DB_USER",
+    "password": "$DB_PASS",
+    "name": "$DB_NAME",
+    "db_file": "data/data.db",
+    "table_prefix": "x_",
+    "ssl_mode": "true"
+  },
+  "scheme": {
+    "https": false,
+    "cert_file": "",
+    "key_file": ""
+  },
+  "temp_dir": "data/temp",
+  "bleve_dir": "data/bleve",
+  "log": {
+    "enable": true,
+    "name": "data/log/log.log",
+    "max_size": 10,
+    "max_backups": 5,
+    "max_age": 28,
+    "compress": false
+  },
+  "max_connections": 0,
+  "tls_insecure_skip_verify": false
+}
+EOF
+
+npm start &
+./main admin 
